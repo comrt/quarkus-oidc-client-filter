e5e7a1ab03fbd2b8c0b68e44950a5a6d4d1d3308
Reichert, Timo <timo.reichert@debeka.de>
2025-07-11 17:33:21 +0200

    more oidc config

--- src/test/java/com/rt/quarkus/ExampleResourceIT.java
+++ src/test/java/com/rt/quarkus/ExampleResourceIT.java
@@ -1,8 +1,22 @@
 package com.rt.quarkus;
 
+import io.quarkus.test.common.WithTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusIntegrationTest
-class ExampleResourceIT extends ExampleResourceTest {
-  // Execute the same tests but in packaged mode.
+@WithTestResource(UpstreamServerMocks.class)
+class ExampleResourceIT {
+
+  @Test
+  void testHelloEndpoint() {
+    given()
+        .when().get("/hello")
+        .then()
+        .statusCode(200)
+        .body(is("Error calling API: HTTP 404 Not Found"));
+  }
 }
--- src/test/java/com/rt/quarkus/UpstreamServerMocks.java
+++ src/test/java/com/rt/quarkus/UpstreamServerMocks.java
@@ -10,7 +10,9 @@ public class UpstreamServerMocks implements QuarkusTestResourceLifecycleManager 
   @Override
   public Map<String, String> start() {
     Map<String, String> config = new HashMap<>();
-    config.put("quarkus.oidc-client.client-id", "quarkus-app");
+    config.put("quarkus.oidc-client.client-id", "custom-oidc-client-id");
+    config.put("quarkus.oidc-client.credentials.secret", "custom-secret");
+    config.put("quarkus.oidc-client.auth-server-url", "${keycloak.url:replaced-by-test-resource}/realms/test-realm");
     config.put("quarkus.rest-client.\"api-client\".url", "http://localhost:8081");
     return config;
   }
